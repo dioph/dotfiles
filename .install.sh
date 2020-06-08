b9a948acdab31f3b0304aa06c1f309a7ce010a8c
dioph <dioph@protonmail.com>
2020-06-08 11:27:21 -0300

    fix winehq add-repository

--- .install.sh
+++ .install.sh
@@ -72,7 +72,7 @@ fi
 # install wine staging
 sudo dpkg --add-architecture i386
 wget -qO - https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
-sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubu... eoan main'
+sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
 sudo apt-get update
 sudo apt-get install --install-recommends wine-staging
 sudo apt-get install libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386
